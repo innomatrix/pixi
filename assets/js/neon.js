8ff3987abd854ed04bd2c39008abff5ed988dace
shadaxv <aleksandern1116@gmail.com>
2018-10-02 20:07:42 +0200

    Fixed neon ratio

--- assets/js/neon.js
+++ assets/js/neon.js
@@ -17,6 +17,7 @@ export default class neon {
     this.bg = PIXI.Sprite.fromImage('assets/images/showdown/showdown-off.png');
 
     this.bg.anchor.set(0.5);
+
     this.bg.ratio = this.initH * 2.4 / this.initH;
 
     this.bg.x = this.app.screen.width / 2;
@@ -27,9 +28,11 @@ export default class neon {
     window.addEventListener('resize', this._l);
 
     this.app.stage.addChild(this.bg);
+    this.firstResize = false;
   }
 
   destroy() {
+
     window.removeEventListener('load', this._l);
     window.removeEventListener('resize', this._l);
 
@@ -43,15 +46,30 @@ export default class neon {
   }
 
   resize() {
-    const calcInnerHeight = window.innerHeight * 2.4 / 5;
-    const calcInnerWidth = window.innerWidth * 0.9;
-    const width = Math.max(Math.min(calcInnerWidth, calcInnerHeight), 240);
-    const height = Math.max(Math.min(calcInnerHeight > calcInnerWidth ? calcInnerWidth / 2.4 : calcInnerHeight / 2.4, window.innerHeight * 2.4 / 5.5), 100);
-    const ratio = height / width;
-    this.bg.scale.x = this.bg.scale.y = 0.2;
-    this.app.renderer.resize(width, height);
-    this.bg.position.set(this.app.screen.width / 2, this.app.screen.height / 2);
-
+    if (!this.firstResize) {
+      this.backgroundSize = {
+        'width': this.bg.width,
+        'height': this.bg.height
+      }
+    }
+    const parent = this;
+    setTimeout(function() {
+      const calcInnerHeight = window.innerHeight * 2.4 / 5;
+      const calcInnerWidth = window.innerWidth * 0.9;
+      const width = Math.max(Math.min(calcInnerWidth, calcInnerHeight), 240);
+      const height = Math.max(Math.min(calcInnerHeight > calcInnerWidth ? calcInnerWidth / 2.4 : calcInnerHeight / 2.4, window.innerHeight * 2.4 / 5.5), 100);
+      const ratio = height / width;
+      const containerSize = {
+        'width': $('#neon-container').width(),
+        'height': $('#neon-container').height()
+      };
+      const scale = Math.min(containerSize.width / parent.backgroundSize.width, containerSize.height / parent.backgroundSize.height) * 0.8;
+      parent.bg.scale.x = parent.bg.scale.y = scale;
+      parent.app.renderer.resize(width, height);
+      console.log(width, height);
+      parent.bg.position.set(parent.app.screen.width / 2, parent.app.screen.height / 2);
+    }, 10);
+    this.firstResize = true;
   }
 
 }
